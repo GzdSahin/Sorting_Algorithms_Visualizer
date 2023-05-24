4dd5d722bafdeb76961297217c7c249a0d279ca4
Buse Çetin <busecetin587@gmail.com>
2023-05-24 16:52:27 +0300

    designer

--- Sorting_Algorithms_Visualizer/deneme.py
+++ Sorting_Algorithms_Visualizer/deneme.py
@@ -8,19 +8,18 @@ from sorting import Sortings
 Sortings=Sortings()
 
 root = Tk()
-root.title('Sorting Algorithm Visualiser')
+root.title('Sorting Algorithm')
 root.maxsize(900, 600)
-root.config(bg='black')
+root.config(bg='white')
 
 #variables
 selected_alg = StringVar()
 data = []
-
 #function
 def drawData(data, colorArray):
     canvas.delete("all")
-    c_height = 380
-    c_width = 600
+    c_height = 300
+    c_width = 300
     x_width = c_width / (len(data) + 1)
     offset = 30
     spacing = 10
@@ -76,34 +75,34 @@ def StartAlgorithm():
 
 
 #frame / base lauout
-UI_frame = Frame(root, width= 700, height=300, bg='black')
+UI_frame = Frame(root, width= 700, height=300, bg='white')
 UI_frame.grid(row=0, column=0, padx=0, pady=0)
 
-canvas = Canvas(root, width=700, height=380, bg='black')
-canvas.grid(row=1, column=0, padx=0, pady=0)
+canvas = Canvas(root, width=700, height=380, bg='white')
+canvas.grid(row=0, column=1, padx=0, pady=0)
 
 #User Interface Area
 #Row[0]
-Label(UI_frame, text="Algorithm: ", bg='black',fg='white').grid(row=0, column=0, padx=5, pady=5, sticky=W)
+Label(UI_frame, text="Algorithm: ", bg='white',fg='white').grid(row=0, column=0, padx=5, pady=5, sticky=W)
 algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=['Bubble Sort','Selection Sort' ,'Quick Sort', 'Merge Sort'])
-algMenu.grid(row=0, column=1, padx=5, pady=5)
+algMenu.grid(row=0, column=0, padx=5, pady=5)
 algMenu.current(0)
 
-speedScale = Scale(UI_frame, from_=0.1, to=5.0, length=200, digits=2, resolution=0.2, orient=HORIZONTAL,bg='white', label="Select Speed [s]")
-speedScale.grid(row=0, column=2, padx=5, pady=5)
-Button(UI_frame, text="Start", command=StartAlgorithm, bg='green').grid(row=1, column=3, padx=5, pady=5)
+speedScale = Scale(UI_frame, from_=0.1, to=5.0, length=100, digits=2, resolution=0.2, orient=HORIZONTAL,bg='white', label="Select Speed [s]")
+speedScale.grid(row=1, column=0, padx=5, pady=5)
+Button(UI_frame, text="Start", command=StartAlgorithm, bg='green').grid(row=6, column=0, padx=5, pady=5)
 
 #Row[1]
 sizeEntry = Scale(UI_frame, from_=3, to=25, resolution=1, orient=HORIZONTAL, bg='white',label="Data Size")
-sizeEntry.grid(row=1, column=0, padx=5, pady=5)
+sizeEntry.grid(row=2, column=0, padx=5, pady=5)
 
 minEntry = Scale(UI_frame, from_=0, to=10, resolution=1, orient=HORIZONTAL, label="Min Value",bg='white')
-minEntry.grid(row=1, column=1, padx=5, pady=5)
+minEntry.grid(row=3, column=0, padx=5, pady=5)
 
 maxEntry = Scale(UI_frame, from_=10, to=100, resolution=1, orient=HORIZONTAL, label="Max Value",bg='white')
-maxEntry.grid(row=1, column=2, padx=5, pady=5)
+maxEntry.grid(row=4, column=0, padx=5, pady=5)
 
-Button(UI_frame, text="Generate", command=Generate, bg='grey').grid(row=0, column=3, padx=5, pady=5)
+Button(UI_frame, text="Generate", command=Generate, bg='grey').grid(row=5, column=0, padx=5, pady=5)
 
 root.mainloop()
 
